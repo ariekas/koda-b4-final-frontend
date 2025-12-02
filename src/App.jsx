647cae181e61c043873ccf8e4a06fe0d39559c51
ariekas <ariariekas@gmail.com>
2025-12-02 21:23:15 +0700

    feat: create register and integrasion back end auth register

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { createBrowserRouter, RouterProvider} from "react-router-dom"
 import Login from "./pages/auth/login"
+import Register from "./pages/auth/register"
+import HomePage from "./pages/home"
+import Dashboard from "./pages/dashboard"
+import ListLink from "./pages/dashboard/list"
+import Profile from "./pages/dashboard/profile"
 
 const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  },
+  {
+    path: "/dashboard/list",
+    element: <ListLink />
+  },
+  {
+    path: "/dashboard/profile",
+    element: <Profile />
   },
 ])
 function App() {
